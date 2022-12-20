a2f306e470418d073c3d8bb426c0b6ae7637fb28
ggozturkmen <gulcangokhan02@gmail.com>
2022-12-19 20:14:43 -0600

    The feature of the car have been added successfully

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        CarFeatures XLR= new CarFeatures();
+
+        CarFeatures XLR = new CarFeatures();
         System.out.println("launch the car");
+        XLR.color = "red";
+        XLR.doors = "5";
+        XLR.engine = 32323;
+        XLR.model = "a5";
     }
 }
